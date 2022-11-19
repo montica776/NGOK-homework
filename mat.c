4b234caa2b38a43a5498b32d2d6a1d1da8577844
montica776 <116187958+montica776@users.noreply.github.com>
2022-11-19 13:39:59 +0700

    Update mat.c

--- mat.c
+++ mat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
-#define SIZE_ROMAPEDIK 5
-#define SIZE_ROMAPEDIGRI 5
+#define SIZE_ROMAPEDIK 3
+#define SIZE_ROMAPEDIGRI 3
 {
     int romansu[5][5];
     int i, j;
@@ -20,14 +20,38 @@ int main()
         }
     }
     printf("\n");
-    int romansy = 0;
+    int ch = 0;
     int kakaska = 0;
+    int nechetkiy = 0;
+    int ne = 0;
+    int sumkakashka = 0;
+    int sumne = 0;
     for (i = 0; i < SIZE_ROMAPEDIK; i++) {
         for (j = 0; j < SIZE_ROMAPEDIGRI; j++) {
             if (romansu[i][j] % 2 == 0) {
-                int romansy = &romansy[i][j];
+                ch = romansu[i][j];
                 kakaska += 1;
+                sumkakashka += romansu[i][j];
+            } else {
+                romansu[i][j];
+                ne += 1;
+                sumne += romansu[i][j];
             }
+        }
+    }
+    printf("Колличество чётных элементов = %d", kakaska);
+    printf("\n");
+    printf("Колличество сумм чётных элементов = %d", sumkakashka);
+    printf("\n");
+    printf("Колличество нечетных чисел = %d", ne);
+    printf("\n");
+    printf("Колличество сумм нечетных чисел = %d", sumne);
+    printf("\n");
+
+    for (i = SIZE_ROMAPEDIK - 1; i >= 0; i--) {
+        printf("\n");
+        for (j = SIZE_ROMAPEDIGRI - 1; j >= 0; j--) {
+            printf(" %d ", romansu[i][j]);
         }
     }
 }
